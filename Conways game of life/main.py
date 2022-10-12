8136588eab4b82c54ebe18e351e3d072a9038bd5
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-10-12 09:11:30 -0400

    Create main.py
    unfinished

---
+++ Conways game of life/main.py
@@ -0,0 +1,44 @@
+# imports
+import time
+import sys
+import os
+
+# get board
+with open(sys.argv[1], "r") as f:
+  board = list(f.read())
+  board_cp = board
+
+
+# draw board
+def drawboard():
+  for y in board:
+    out = ""
+
+    for n in y:
+      if board[board.index(y)][board[board.index(y)].index(n)]:
+        out += "#"
+
+      else:
+        out += "."
+
+    print(out)
+
+
+# update
+def update():
+  pass
+
+
+# main function
+def main():
+  # loop
+  while 1:
+    update()
+    drawboard()
+    time.sleep(0.5)
+    os.system("cls" if os.name == 'nt' else "clear")
+
+
+# if imported
+if __name__ == "__main__":
+  main()
